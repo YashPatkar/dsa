ca8ddb07cec4276a981049da9eaaa1ede0225b63
Yash Patkar <yash.patkar2004@gmail.com>
2024-07-19 22:19:23 +0530

    Solved array question: 496. Next Greater Element I( leetcode )

--- Arrays/question496.java
+++ Arrays/question496.java
@@ -1,40 +1,47 @@
 // Question: 496. Next Greater Element I
 // URL: https://leetcode.com/problems/next-greater-element-i/description/
+
+import java.util.*;
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        for(int i = 0; i < nums1.length; i++)
-        {
-            check(i, nums1, nums2);
-        }
-        return nums1;
-    }
-    public static void check(int index, int nums1[], int nums2[])
-    {
-        if(index == nums2.length - 1)
-        {
-            nums1[index] = -1;
-        }
-        else
-        {
-            int j = 0;
-            for(int i = 0; i < nums2.length; i++)
-            {
-                if(nums1[i] < nums2[j])
-                {
-                    nums1[i] = nums2[j];
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < nums1.length; i++) {
+            boolean found = false;
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    if (j != (nums2.length - 1)) {
+                        for (int k = j + 1; k < nums2.length; k++) {
+                            if (nums2[k] > nums2[j]) {
+                                nums.add(Integer.valueOf(nums2[k]));
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (found == false) {
+                            nums.add(-1);
+                        }
+                    } else {
+                        nums.add(-1);
+                    }
                     break;
                 }
-                j++;
             }
         }
+        int a[] = new int[nums.size()];
+        for (int i = 0; i < nums.size(); i++) {
+            a[i] = nums.get(i);
+        }
+        return a;
     }
 }
+
 
 class question496{
     public static void main(String s[])
     {
         int nums1[]={ 4, 1, 2};
         int nums2[] = {1,3,4,2};
-        System.out.println(new Solution().nextGreaterElement(nums1, nums2));
+        int a[] = new Solution().nextGreaterElement(nums1, nums2);
+        for(int i : a) System.out.print(i + " ");
     }
 }
